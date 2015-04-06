51ebbec023a5d88b8a05c77572615070ada22e27
Pat Eason <patrick@pateason.com>
2015-04-05 22:26:38 -0400

    hero styles and animation

--- header.php
+++ header.php
@@ -6,15 +6,17 @@
     <title><?php wp_title(); ?></title>
     <?php wp_head(); ?>
   </head>
-  <body>
+  <body <?php body_class();?>>
     <?php echo get_template_part('partials/nav', 'cover'); ?>
 
-
     <header>
-
-    </header>
-    <section id="hero">
       <div class="row">
-          <?php echo get_avatar(get_option('admin_email'), 256, '', 'Pat Eason\'s ugly mug'); ?>
+        <div class="column small-12 medium-5 hero-img">
+          <?php echo get_avatar(get_option('admin_email'), 160, '', 'Pat Eason\'s ugly mug'); ?>
+        </div>
+        <div class="column small-12 medium-7 hero-tag">
+          <h1>Pat Eason</h1>
+          <h4>Frontend Developer</h4>
+        </div>
       </div>
-    </section>
+    </header>
