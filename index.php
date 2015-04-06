089df583fb93a55fb073daa15cc0e540acb85474
Pat Eason <patrick@pateason.com>
2015-04-05 22:59:16 -0400

    hero mobile styling and post module fadein

--- index.php
+++ index.php
@@ -1,5 +1,36 @@
 <?php get_header(); ?>
 
-index.php
+<section id="posts">
+  <div class="row">
+    <?php for($x=0;$x<=10;$x++){
+      $class='';
+      if($x == 0){
+        $class="post-one";
+      }
+      if($x == 1){
+        $class="post-two";
+      }
+      if($x == 2){
+        $class="post-three";
+      } ?>
 
+      <div class="column small-12 medium-4 post-entry <?php echo $class; ?>">
+        <div class="post-gutter">
+          <h4 class="post-title"><a href="#">Post Title</a></h4>
+          <hr>
+          <span class="post-excerpt">Here is a post excerpt that will be used on the site whenever I start making posts and people will love it, why wouldn't they? It's a brilliant idea to get all this together [...]</span>
+          <hr>
+          <div class="post-meta">
+            <div class="post-tags">
+              <a class="label">WordPress</a>
+              <a class="label">Javascript</a>
+              <a class="label">Tags</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    <?php } ?>
+  </div>
+</section>
 <?php get_footer(); ?>
